9b6ffd1fa2143b69f45d8482c408c08c19162349
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2025-05-22 02:42:43 +0000

    Combining list into destination card

--- components/HomeScreenComponents/DestinationCardComponent.tsx
+++ components/HomeScreenComponents/DestinationCardComponent.tsx
@@ -5,27 +5,50 @@ type Props = {
     city: string;
     country: string;
     rating: number;
+    listComponent?: boolean;
 }
 
-export default function DestinationCard() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image 
-                    style={styles.image}
-                    source={{
-                        uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3iceZdrJZJhEc5hf40Zw2NPCwlSUScWLOQ&s'
-                    }}
-                />
-                <View style={styles.ratingContainer}>
-                    <Text>⭐ 4.8</Text>
-                </View>
-            </View>
-            <View style={styles.textContainer}>
-                <Text style={styles.cityText}>Dubai</Text>
-                <Text style={styles.countryText}>United Arab Emirates</Text>
-            </View>
-        </View>
+export default function DestinationCard({ listComponent }: Props) {
+        return (
+            <>
+                {listComponent ? (
+                    <View style={styles.listItemContainer}>
+                        <View style={{width: 70, height: 70,}}>
+                            <Image 
+                                style={styles.image}
+                                source={{
+                                    uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3iceZdrJZJhEc5hf40Zw2NPCwlSUScWLOQ&s'
+                                }}
+                            />
+                        </View>
+                        <View style={{width: '55%'}}>
+                            <Text style={styles.cityText}>Dubai</Text>
+                            <Text style={styles.countryText}>United Arab Emirates</Text>
+                        </View>
+                        <View>
+                            <Text style={{fontFamily: 'JakartaMed', fontSize: 14}}>⭐ 4.8</Text>
+                        </View>
+                    </View>
+                ) : (
+                    <View style={styles.container}>
+                        <View style={styles.imageContainer}>
+                            <Image 
+                                style={styles.image}
+                                source={{
+                                    uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3iceZdrJZJhEc5hf40Zw2NPCwlSUScWLOQ&s'
+                                }}
+                            />
+                            <View style={styles.ratingContainer}>
+                                <Text>⭐ 4.8</Text>
+                            </View>
+                        </View>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.cityText}>Dubai</Text>
+                            <Text style={styles.countryText}>United Arab Emirates</Text>
+                        </View>
+                    </View>
+                )}
+            </>
     );
 }
 
@@ -41,6 +64,17 @@ const styles = StyleSheet.create({
         gap: 15,
         paddingRight: 20,
     },
+    listItemContainer: {
+        backgroundColor: '#FFFFFF',
+        width: 327,
+        height: 90,
+        borderRadius: 18,
+        padding: 8,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+    }, 
     imageContainer: {
         width: 180,
         height: 220,
